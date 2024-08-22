b1018a8aa8d4fe2c5ed0b9e9559f3195c6711801
Oleg Prikhodko <4.oleg.pr@gmail.com>
2024-08-22 10:40:05 +0500

    style: modify options.lua and add gitignore

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,27 +1,23 @@
---local opt = vim.opt
+local opt = vim.opt
 
 -- Text formatting
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+opt.expandtab = true
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
 
---vim.cmd("set tabstop=2")
---vim.cmd("set softtabstop=2")
---vim.cmd("set shiftwidth=2")
---vim.g.mapleader = " "
+
+
 --vim.g.background = "light"
-
 --vim.opt.swapfile = false
 
 -- Navigate vim panes better
---vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
---vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
---vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
---vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 
---vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
---vim.wo.number = true
 
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -29,15 +25,12 @@ vim.opt.clipboard = "unnamedplus"
 --vim.opt.timeoutlen = 100
 
 
---vim.g.mapleader = " "
 
 
---vim.g.mapleader = " "
 
 --vim.opt.encoding = "utf-8"
 --vim.opt.fileencoding = "utf-8"
 
-vim.opt.number = true
 
 vim.opt.title = true
 vim.opt.autoindent = true
@@ -45,37 +38,34 @@ vim.opt.smartindent = true
 vim.opt.hlsearch = true
 vim.opt.backup = false
 vim.opt.showcmd = true
-vim.opt.cmdheight = 1
-vim.opt.laststatus = 3
-vim.opt.expandtab = true
-vim.opt.scrolloff = 10
-vim.opt.shell = "fish"
-vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
-vim.opt.inccommand = "split"
-vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
-vim.opt.smarttab = true
-vim.opt.breakindent = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.wrap = false -- No Wrap lines
-vim.opt.backspace = { "start", "eol", "indent" }
-vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
-vim.opt.wildignore:append({ "*/node_modules/*" })
-vim.opt.splitbelow = true -- Put new windows below current
-vim.opt.splitright = true -- Put new windows right of current
-vim.opt.splitkeep = "cursor"
-vim.opt.mouse = ""
+--vim.opt.cmdheight = 1
+--vim.opt.laststatus = 3
+--vim.opt.scrolloff = 10
+--vim.opt.shell = "fish"
+--vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
+--vim.opt.inccommand = "split"
+--vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
+--vim.opt.smarttab = true
+--vim.opt.breakindent = true
+vim.opt.wrap = true
+--vim.opt.backspace = { "start", "eol", "indent" }
+--vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
+--vim.opt.wildignore:append({ "*/node_modules/*" })
+--vim.opt.splitbelow = true -- Put new windows below current
+--vim.opt.splitright = true -- Put new windows right of current
+--vim.opt.splitkeep = "cursor"
+--vim.opt.mouse = ""
 
 -- Undercurl
-vim.cmd([[let &t_Cs = "\e[4:3m"]])
-vim.cmd([[let &t_Ce = "\e[4:0m"]])
+-- vim.cmd([[let &t_Cs = "\e[4:3m"]])
+-- vim.cmd([[let &t_Ce = "\e[4:0m"]])
 
 -- Add asterisks in block comments
-vim.opt.formatoptions:append({ "r" })
+-- vim.opt.formatoptions:append({ "r" })
 
-vim.cmd([[au BufNewFile,BufRead *.astro setf astro]])
-vim.cmd([[au BufNewFile,BufRead Podfile setf ruby]])
+-- vim.cmd([[au BufNewFile,BufRead *.astro setf astro]])
+-- vim.cmd([[au BufNewFile,BufRead Podfile setf ruby]])
 
-if vim.fn.has("nvim-0.8") == 1 then
-	vim.opt.cmdheight = 0
-end
+-- if vim.fn.has("nvim-0.8") == 1 then
+	-- vim.opt.cmdheight = 0
+-- end
