5164e05bbaef34141a558e25f555cdf79ba70732
Oleg Prikhodko <4.oleg.pr@gmail.com>
2024-09-02 23:08:26 +0500

    refactor: add mario plugins to my config

--- nvim/.config/nvim/lua/plugins/telescope-mario.lua
+++ nvim/.config/nvim/lua/plugins/telescope-mario.lua
@@ -56,5 +56,11 @@ return {
         map("n", "<leader>ff", builtin.find_files, opts) -- Lists files in your current working directory, respects .gitignore
         map("n", "<leader>fx", builtin.treesitter, opts) -- Lists tree-sitter symbols
         map("n", "<leader>fs", builtin.spell_suggest, opts) -- Lists spell options
+
+        map("n", "<leader>fg", builtin.live_grep, opts)
+        map("n", "<leader>fb", builtin.buffers, opts)
+        map("n", "<leader>fh", builtin.help_tags, opts)
+   
+    
     end
 }
