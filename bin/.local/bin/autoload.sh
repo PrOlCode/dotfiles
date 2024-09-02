11a592e21319035aa3dda3e098b0c8e9817f3e88
Oleg Prikhodko <4.oleg.pr@gmail.com>
2024-09-02 17:15:35 +0500

    refactor: add mario options

--- bin/.local/bin/autoload.sh
+++ bin/.local/bin/autoload.sh
@@ -4,4 +4,6 @@ alacritty -e tmux & disown
 # google-chrome-stable &
 # obsidian &
 
-#xset r rate 300 50
+# xset r rate repeat_delay repeat_speec (by default 500 20)
+# xset r rate 300 50
+xset r rate 350 50
--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -2,3 +2,4 @@ local options = { noremap = true, silent = true }
 -- vim.keymap.set("i", "jk", "<Esc>", options)
 -- vim.keymap.set("i", "ол", "<Esc>", options)
 vim.keymap.set("n", "<Leader>w", ":w<CR>", options)
+vim.keymap.set("n", "<Leader>c", ":bdelete<CR>", options)
