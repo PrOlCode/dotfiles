baeb95a072fb8d7a1553db1126faa69413b62064
Oleg Prikhodko <4.oleg.pr@gmail.com>
2024-09-03 23:14:04 +0500

    feat: add nohighligh

--- bin/.local/bin/autoload.sh
+++ bin/.local/bin/autoload.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
+wmctrl -s 2
 alacritty -e tmux & disown
 #brave &
 # google-chrome-stable &
 # obsidian &
-
-# xset r rate repeat_delay repeat_speec (by default 500 20)
-# xset r rate 300 50
 xset r rate 350 50
