c0bf5e317637c9f0c6f3dfb33a6268537df3fc22
Oleg Prikhodko <4.oleg.pr@gmail.com>
2024-09-03 08:39:52 +0500

    style: delete close buffer key-mapping

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -2,4 +2,3 @@ local options = { noremap = true, silent = true }
 -- vim.keymap.set("i", "jk", "<Esc>", options)
 -- vim.keymap.set("i", "ол", "<Esc>", options)
 vim.keymap.set("n", "<Leader>w", ":w<CR>", options)
-vim.keymap.set("n", "<Leader>c", ":bdelete<CR>", options)
