e7cb189cd511192149deed14747b3a9a9f4b7eec
Oleg Prikhodko <4.oleg.pr@gmail.com>
2024-09-06 10:42:56 +0500

    style: rename  plugins

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,5 +1,3 @@
 local options = { noremap = true, silent = true }
--- vim.keymap.set("i", "jk", "<Esc>", options)
--- vim.keymap.set("i", "ол", "<Esc>", options)
 vim.keymap.set("n", "<Leader>w", ":w<CR>", options)
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', options)
