8cfdb583e921132e02d21ea16e2885c3c2cae51a
Oleg Prikhodko <4.oleg.pr@gmail.com>
2024-09-07 11:06:29 +0500

    feat: add none-ls plugin

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -10,41 +10,27 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		lazy = false,
 		opts = {
-			-- ensure_installed = { "lua_ls", "gopls", "pyright", "markdown_oxide" },
 			ensure_installed = { "lua_ls", "gopls", "pyright", "harper_ls" },
 			automatic_installation = true,
 		},
 	},
-
-
-
 	{
 		"neovim/nvim-lspconfig",
 		lazy = false,
 		config = function()
 			-- local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			-- local lspconfig = require("lspconfig")
-			-- 
-			-- lspconfig.lua_ls.setup({
-			-- 	capabilities = capabilities,
-			-- })
-			-- lspconfig.gopls.setup({
-			-- 	capabilities = capabilities,
-			-- })
-			-- lspconfig.pyright.setup({
-			-- 	capabilities = capabilities,
-			-- })
-			-- lspconfig.marksman.setup({
-			-- 	capabilities = capabilities,
-			-- })
-			--
-			-- vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			-- vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			-- vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			-- vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.gopls.setup({})
+			lspconfig.pyright.setup({})
+			-- lspconfig.harper_ls.setup({})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},
-
-
-  
 }
+-- TODO :h lsp.lsp.buf - help for lsp buf section
+-- TODO compare plugin telescope UI
+-- TODO Ctrl+X + Ctrl+O => 15:50
--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -6,15 +6,14 @@ return {
       sources = {
         -- null_ls.builtins.diagnostics.selene,
         -- null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.golangci_lint,
-        null_ls.builtins.formatting.golines,
+        -- null_ls.builtins.diagnostics.golangci_lint,
+        -- null_ls.builtins.formatting.golines,
         -- null_ls.builtins.diagnostics.pylint,
         -- null_ls.builtins.formatting.black,
         -- null_ls.builtins.diagnostics.markdownlint,
         -- null_ls.builtins.formatting.prettier,
       },
     })
-
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
   end,
 }
