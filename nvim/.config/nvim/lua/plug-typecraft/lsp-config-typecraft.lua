b1fa57664eba37145a708a2d329384bea4bedfd4
Oleg Prikhodko <4.oleg.pr@gmail.com>
2024-09-07 13:44:46 +0500

    feat: add mason-tool-instaler plugin

--- nvim/.config/nvim/lua/plug-typecraft/lsp-config-typecraft.lua
+++ nvim/.config/nvim/lua/plug-typecraft/lsp-config-typecraft.lua
@@ -20,7 +20,6 @@ return {
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
-			
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
 			})
--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,19 +1,36 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        -- null_ls.builtins.diagnostics.selene,
-        null_ls.builtins.formatting.stylua,
-        -- null_ls.builtins.diagnostics.golangci_lint,
-        -- null_ls.builtins.formatting.golines,
-        -- null_ls.builtins.diagnostics.pylint,
-        -- null_ls.builtins.formatting.black,
-        -- null_ls.builtins.diagnostics.markdownlint,
-        -- null_ls.builtins.formatting.prettier,
-      },
-    })
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+  {
+    "nvimtools/none-ls.nvim",
+    config = function()
+      local null_ls = require("null-ls")
+      null_ls.setup({
+        sources = {
+          -- null_ls.builtins.diagnostics.selene, -- lua linter
+          null_ls.builtins.formatting.stylua,    -- lua formatter
+          null_ls.builtins.diagnostics.golangci_lint, -- go linter
+          null_ls.builtins.diagnostics.pylint,   -- python linter
+          null_ls.builtins.formatting.black,     -- python formatter
+        },
+      })
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    requires = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-tool-installer").setup({
+        ensure_installed = {
+          "selene",
+          "stylua",
+          "golangci-lint",
+          "pylint",
+          "black",
+        },
+        auto_update = false,
+        run_on_start = true,
+        start_delay = 3000,
+      })
+    end,
+  },
 }
