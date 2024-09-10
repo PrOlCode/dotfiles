893c99193deed56475228e3fde59658b27bc2094
Oleg Prikhodko <4.oleg.pr@gmail.com>
2024-09-10 08:06:33 +0500

    feat: add telescope-file-browser plugin

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,9 +2,14 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-file-browser.nvim",
+    },
     config = function()
       local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "-", ":Telescope file_browser<CR>")
+
       vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -77,7 +77,7 @@ ZSH_THEME="robbyrussell"
 # ???Подключение fzf-tab
 # ???source ~/.oh-my-zsh/custom/plugins/fzf-tab/fzf-tab.plugin.zsh
 
-plugins=(git zsh-autosuggestions)
+plugins=(git vi-mode zsh-autosuggestions)
 
 source $ZSH/oh-my-zsh.sh
 
