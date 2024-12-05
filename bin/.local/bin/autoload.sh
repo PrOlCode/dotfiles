ced13445f210ce3574f16aee2524989d111a1032
Oleg Prikhodko <4.oleg.pr@gmail.com>
2024-12-05 13:22:45 +0500

    refactor: change autoload

--- bin/.local/bin/autoload.sh
+++ bin/.local/bin/autoload.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 wmctrl -s 2
-alacritty -e tmux & disown
-#brave &
-# google-chrome-stable &
-# obsidian &
 xset r rate 350 50
+alacritty -e tmux & disown
+yandex-browser-stable & disown
